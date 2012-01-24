5c1fa06196f348fc4dd713845a2af4f0ec4775ff
elanz <elanz1615@yahoo.com>
2012-01-24 15:53:03 -0500

    initial commit

---
+++ MiniRaiders/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MiniRaiders
+//
+//  Created by elanz on 1/24/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
